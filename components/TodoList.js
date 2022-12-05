e1ae225affd253053ae88e37651848ced777a3e6
wonjuYang <his1monologue@gmail.com>
2022-12-05 23:31:57 +0900

    Initial commit!

---
+++ components/TodoList.js
@@ -0,0 +1,20 @@
+// components/TodoList.js
+import React from 'react';
+import {StyleSheet, ScrollView, Text} from 'react-native';
+import TodoListItem from './TodoListItem';
+
+const TodoList = () => {
+  return (
+    <ScrollView contentContainerStyle={styles.listContainer}>
+      <TodoListItem />
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  listContainer: {
+    alignItems: 'center',
+  },
+});
+
+export default TodoList;
